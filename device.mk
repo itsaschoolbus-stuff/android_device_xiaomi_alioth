5e19c99182c7721e4ff20d0417833285d7bd4649
Dyneteve <dyneteve@hentaios.com>
2022-01-07 19:23:41 +0530

    alioth: Import regulatory info
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- device.mk
+++ device.mk
@@ -44,6 +44,11 @@ PRODUCT_PACKAGES += \
     LineageSystemUIOverlayDevice \
     SystemUIOverlayDevice
 
+# RRO Overlays
+PRODUCT_PACKAGES += \
+    SettingsOverlayM2012K11AG \
+    SettingsOverlayM2012K11AI
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 30
 
