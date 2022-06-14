e8377fb774d850d1abc0f335d1d30c8844bd3cde
Omkar Chandorkar <gotenksIN@aosip.dev>
2022-06-14 14:40:32 +0530

    alioth: Ship camera config in tree
    - From Miui Alioth 14.0.8.0 Mi
    
    Co-authored-by: johnmart19 <johnivan19999@gmail.com>

--- device.mk
+++ device.mk
@@ -28,6 +28,10 @@ PRODUCT_PACKAGES += \
 
 $(call inherit-product-if-exists, device/xiaomi/camera/miuicamera.mk)
 
+# Miui Camera STLicense
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/camera/st_license.lic:$(TARGET_COPY_OUT_VENDOR)/etc/camera/st_license.lic
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
