ac4b867ad86eafc9d58537b3777b32eb6178d418
bxySo <bxyso@outlook.com>
2024-12-19 01:06:04 +0800

    alioth: overlay: Import regulatory info for M2012K11AC

--- device.mk
+++ device.mk
@@ -46,6 +46,7 @@ PRODUCT_PACKAGES += \
 
 # RRO Overlays
 PRODUCT_PACKAGES += \
+    SettingsOverlayM2012K11AC \
     SettingsOverlayM2012K11AG \
     SettingsOverlayM2012K11AI \
     SettingsProviderM2012K11AC \
